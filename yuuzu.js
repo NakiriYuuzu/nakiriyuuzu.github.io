f672d0600cc934956354863a8d168739ac620fac
Yuuzu <s1082978@gm.pu.edu.tw>
2022-10-29 23:21:09 +0800

    Update to Vue.js

---
+++ yuuzu.js
@@ -0,0 +1,96 @@
+﻿new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        countDate: 0,
+    },
+    created: function () {
+        this.countDate = this.checkDate()
+        this.getCost()
+        this.countDown()
+    },
+    watch: {},
+    methods: {
+        getCost: function() {
+            this.$http.get('http://192.168.31.74:8000/api/Cost/')
+                .then(function (response) {
+                    response.body.forEach((item => {
+                        this.message += item.name + ': ' + item.cost + '\n'
+                    }))
+                    console.log(this.message)
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        },
+        
+        checkDate: function () {
+            if (new Date().getMonth() + 1 < 13)
+                return (new Date(this.checkMonth(new Date().getMonth() + 1)).getTime() < new Date().getTime()) ? new Date().getMonth() + 2 : new Date().getMonth() + 1
+            else
+                return 1
+        },
+
+        checkMonth: function(month) {
+            switch (month) {
+                case 1:
+                    return "Jan 08, 2022 12:00:00"
+                case 2:
+                    return "Feb 08, 2022 12:00:00"
+                case 3:
+                    return "Mar 08, 2022 12:00:00"
+                case 4:
+                    return "Apr 08, 2022 12:00:00"
+                case 5:
+                    return "May 08, 2022 12:00:00"
+                case 6:
+                    return "Jun 08, 2022 12:00:00"
+                case 7:
+                    return "Jul 08, 2022 12:00:00"
+                case 8:
+                    return "Aug 08, 2022 12:00:00"
+                case 9:
+                    return "Sep 08, 2022 12:00:00"
+                case 10:
+                    return "Oct 08, 2022 12:00:00"
+                case 11:
+                    return "Nov 08, 2022 12:00:00"
+                case 12:
+                    return "Dec 08, 2022 12:00:00"
+            }
+        },
+
+        checkCost: function () {
+            Swal.fire({
+                title: 'Youtube Premium Cost',
+                text: this.message,
+                imageUrl: 'payment.png',
+                imageWidth: 250,
+                imageHeight: 250,
+                imageAlt: 'Banking Image',
+            })
+        },
+        
+        countDown: function () {
+            const second = 1000
+            const minute = second * 60
+            const hour = minute * 60
+            const day = hour * 24
+            
+            let countDown = new Date(this.checkMonth(this.countDate)).getTime()
+            let x = setInterval(function () {
+                let now = new Date().getTime()
+                let distance = countDown - now
+
+                document.getElementById("days").innerText = Math.floor(distance / (day))
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour))
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute))
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second)
+
+                if (distance < 0) {
+                    clearInterval(x)
+                }
+            }, 1000)
+        }
+    }
+})
